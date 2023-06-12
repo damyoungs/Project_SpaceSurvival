37747b15102df5d9de152ae249e1467dbd057352
Marriages <125725323+steelfs@users.noreply.github.com>
2023-06-12 15:20:29 +0900

    Enum 추가

--- Assets/Scenes/JY/Script/Inventory.cs
+++ Assets/Scenes/JY/Script/Inventory.cs
@@ -2,7 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Current_Inventory_State
+{
+    Equip,
+    Consume,
+    Etc,
+    Etc2
+}
 public class Inventory : Singleton<Inventory>
 {
-    
+    public GameObject Equip_Inven;
+    public GameObject Consume_Inven;
+    public GameObject Etc_Inven;
+    public GameObject Etc2_Inven;
+
+    Current_Inventory_State state;
+
+    public Current_Inventory_State State
+    {
+        get => state;
+        set
+        {
+            state = value;
+            SetInvenState(state);
+        }
+
+    }
+  
+    public void SwitchTab_To_Equip()
+    {
+        State = Current_Inventory_State.Equip;
+    }
+    public void SwitchTab_To_Consume()
+    {
+        State = Current_Inventory_State.Consume;
+    }
+    public void SwitchTab_To_Etc()
+    {
+        State = Current_Inventory_State.Etc;
+    }
+    public void SwitchTab_To_Etc2()
+    {
+        State = Current_Inventory_State.Etc2;
+    }
+    void SetInvenState(Current_Inventory_State state)
+    {
+        switch (state)
+        {
+            case Current_Inventory_State.Equip:
+                Equip_Inven.SetActive(true);
+                Consume_Inven.SetActive(false);
+                Etc_Inven.SetActive(false);
+                Etc2_Inven.SetActive(false);
+                break;
+            case Current_Inventory_State.Consume:
+                Equip_Inven.SetActive(false);
+                Consume_Inven.SetActive(true);
+                Etc_Inven.SetActive(false);
+                Etc2_Inven.SetActive(false);
+                break;
+            case Current_Inventory_State.Etc:
+                Equip_Inven.SetActive(false);
+                Consume_Inven.SetActive(false);
+                Etc_Inven.SetActive(true);
+                Etc2_Inven.SetActive(false);
+                break;
+            case Current_Inventory_State.Etc2:
+                Equip_Inven.SetActive(false);
+                Consume_Inven.SetActive(false);
+                Etc_Inven.SetActive(false);
+                Etc2_Inven.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
--- Assets/Scenes/JY/Script/UI_Object_Spawner.cs
+++ Assets/Scenes/JY/Script/UI_Object_Spawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UI_Object_Spawner : Singleton<UI_Object_Spawner>
@@ -9,31 +10,31 @@ public class UI_Object_Spawner : Singleton<UI_Object_Spawner>
     public Transform consume_Below;
     public Transform etc_Below;
 
+    public Current_Inventory_State state;
     private int slotMax;
   
-    private int currentSlotCount;
+    private int currentSlotCount = 0;
     public int CurrentSlotCount
     {
         get => currentSlotCount;
         set
         {
             currentSlotCount= value;
-            AddSlot(currentSlotCount);
         }
     }
     private void Start()
     {
-        CurrentSlotCount = 4;
+        AddSlot(state);
     }
-    void AddSlot(int currentSlotCount)
+    void AddSlot(Current_Inventory_State state)
     {
+        CurrentSlotCount += 5;
         for (int i = 0; i < currentSlotCount; i++)
         {
             GameObject slot = Instantiate(Slot);
+            slot.name = $"{slot.name}_{i}";
             slot.transform.SetParent(equip_Below, false);
-
         }
         Canvas.ForceUpdateCanvases();
-
     }
 }
