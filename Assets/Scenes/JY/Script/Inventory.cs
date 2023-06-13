345f89ac34b97a1ca263cafd1851a5fe5ffe5f96
steelfs <steelfs7@gmail.com>
2023-06-13 19:19:39 +0900

    .

--- Assets/Scenes/JY/Script/Inventory.cs
+++ Assets/Scenes/JY/Script/Inventory.cs
@@ -23,18 +23,34 @@ public class Inventory : Singleton<Inventory>
     public void SwitchTab_To_Equip()
     {
         state = Current_Inventory_State.Equip;
+        Equip_Inven.SetActive(true);
+        Consume_Inven.SetActive(false);
+        Etc_Inven.SetActive(false);
+        Etc2_Inven.SetActive(false);
     }
     public void SwitchTab_To_Consume()
     {
         state = Current_Inventory_State.Consume;
+        Equip_Inven.SetActive(false);
+        Consume_Inven.SetActive(true);
+        Etc_Inven.SetActive(false);
+        Etc2_Inven.SetActive(false);
     }
     public void SwitchTab_To_Etc()
     {
         state = Current_Inventory_State.Etc;
+        Equip_Inven.SetActive(false);
+        Consume_Inven.SetActive(false);
+        Etc_Inven.SetActive(true);
+        Etc2_Inven.SetActive(false);
     }
     public void SwitchTab_To_Etc2()
     {
         state = Current_Inventory_State.Etc2;
+        Equip_Inven.SetActive(false);
+        Consume_Inven.SetActive(false);
+        Etc_Inven.SetActive(false);
+        Etc2_Inven.SetActive(true);
     }
     void SetInvenState(Current_Inventory_State state)
     {
