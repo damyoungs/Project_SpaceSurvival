7685e947388b828ead7348e91878a305db7d180a
steelfs <steelfs7@gmail.com>
2023-06-22 22:56:34 +0900

    Update UI_Buttons.cs

--- Assets/Scenes/JY/Script/UI/UI_Buttons.cs
+++ Assets/Scenes/JY/Script/UI/UI_Buttons.cs
@@ -40,6 +40,10 @@ public class UI_Buttons : MonoBehaviour
             objects[i] = Util.FindChild<T>(gameObject, names[i], true);
         }
     }
+    T Get<T>(int index) where T : UnityEngine.Object
+    {
+
+    }
     public void OnBtnClick()
     {
         score ++;
