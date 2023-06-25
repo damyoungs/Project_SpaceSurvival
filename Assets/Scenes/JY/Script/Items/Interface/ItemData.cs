1c6a9dd31b3b7c69a6adf9ffcdda7dc6d7ca077e
steelfs <steelfs7@gmail.com>
2023-06-25 18:09:50 +0900

    .

---
+++ Assets/Scenes/JY/Script/Items/Interface/ItemData.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+enum ItemType
+{
+    Equip,
+    Consume,
+    Etc,
+    Craft
+}
+public interface I_ItemData 
+{
+
+}
