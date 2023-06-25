b9b56e743bfc12ecc03a561edfeded16a822d8c3
steelfs <steelfs7@gmail.com>
2023-06-25 14:40:52 +0900

    에셋임포트

--- Assets/Scenes/JY/Script/Core/ButtonController.cs
+++ Assets/Scenes/JY/Script/Core/ButtonController.cs
@@ -5,30 +5,12 @@ using UnityEngine.InputSystem;
 
 public class ButtonController : TestBase 
 {
-    public void Open_Inventory_Button()
-    {
-        GameManager.Inventory.Open_Inventory();
-    }
-    public void Add_Slot_Button()
-    {
-        GameManager.UI_Spawner.Add_Slot();
-    }
-    public void Equip_Button()
-    {
-        GameManager.Inventory.SwitchTab_To_Equip();
-    }
-    public void Consume_Button()
-    {
-        GameManager.Inventory.SwitchTab_To_Consume();
-    }
-    public void Etc_Button()
-    {
-        GameManager.Inventory.SwitchTab_To_Etc();
-    }
-    public void Craft_Button()
-    {
-        GameManager.Inventory.SwitchTab_To_Craft();
-    }
+    public void Open_Inventory_Button() { GameManager.Inventory.Open_Inventory(); }
+    public void Add_Slot_Button() { GameManager.UI_Spawner.Add_Slot(); }
+    public void Equip_Button() { GameManager.Inventory.SwitchTab_To_Equip(); }
+    public void Consume_Button() { GameManager.Inventory.SwitchTab_To_Consume(); }
+    public void Etc_Button() { GameManager.Inventory.SwitchTab_To_Etc(); }
+    public void Craft_Button() { GameManager.Inventory.SwitchTab_To_Craft(); }
     protected override void Test1(InputAction.CallbackContext obj)
     {
         
