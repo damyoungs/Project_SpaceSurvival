ae0cac0feb97b62ce91c193ee22bb9f315e0604e
steelfs <steelfs7@gmail.com>
2023-06-28 22:27:22 +0900

    Update ToolBar.cs

--- Assets/Scenes/JY/Script/UI/ToolBar.cs
+++ Assets/Scenes/JY/Script/UI/ToolBar.cs
@@ -8,27 +8,25 @@ public class ToolBar : MonoBehaviour, IPointerDownHandler,IDragHandler,IPointerU
 {
     Vector2 distance;
     Vector2 positionBeforeDrag;
-    RectTransform canvasPosition;
     RectTransform Inventory;
     float offsetX = 630.0f;
     float offsetY = 130.0f;
     private void Awake()
     {
-        canvasPosition = transform.parent.parent.GetComponent<RectTransform>();
         Inventory = transform.parent.GetComponent<RectTransform>();
     }
     public void OnPointerDown(PointerEventData eventData)
     {
-        distance = Input.mousePosition - Inventory.position;
+        distance = Input.mousePosition - Inventory.position; //클릭하는순간 eventPosition(마우스위치)와 인벤토리의 피봇간 거리 저장
         positionBeforeDrag = Inventory.position;
     }
-    public void OnDrag(PointerEventData eventData)
+    public void OnDrag(PointerEventData eventData) //
     {
-        Inventory.position = eventData.position - distance;
+        Inventory.position = eventData.position - distance; //이동
     }
 
 
-    public void OnPointerUp(PointerEventData eventData)
+    public void OnPointerUp(PointerEventData eventData)//로컬기준 범위밖으로 나갈시 처음 클릭했던 곳으로 이동
     {
         if (Inventory.localPosition.x > offsetX || Inventory.localPosition.x < -offsetX || Inventory.localPosition.y > offsetY || Inventory.localPosition.y < -offsetY)
         {
