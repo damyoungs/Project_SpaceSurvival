e1ba72ab45fbb3f56fb5818bfc9da5abe8993673
steelfs <steelfs7@gmail.com>
2023-06-29 17:03:16 +0900

    초기화 안되는 버그 수정

--- Assets/Scenes/JY/Script/Items/Consume/HpPotion.cs
+++ Assets/Scenes/JY/Script/Items/Consume/HpPotion.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class HpPotion : ItemBase
 {
-    private void Awake()
-    {
-        
-    }
+
     
 }
--- Assets/Scenes/JY/Script/ObjectPool/Factory.cs
+++ Assets/Scenes/JY/Script/ObjectPool/Factory.cs
@@ -23,7 +23,14 @@ public class Factory : MonoBehaviour
     public static Factory I { get { if (instance == null) Init(); return instance; } }
 
     HpPotionPool hpPotionpool;
-
+    MpPotionPool mpPotionPool;
+    BatPool batPool;
+    BowPool bowPool;
+    CashPool cashPool;
+    DaggerPool daggerPool;
+    TwoHandSwordPool twoHandSwordPool;
+    TwoHandAxePool twoHandAxePool;
+    WandPool wandPool;
     static void Init()
     {
         GameObject go = GameObject.Find("Factory");
@@ -39,8 +46,27 @@ public class Factory : MonoBehaviour
     private void Start()
     {
         hpPotionpool = transform.GetChild(0).GetComponent<HpPotionPool>();
+        mpPotionPool = transform.GetChild(1).GetComponent<MpPotionPool>();
+        cashPool = transform.GetChild(2).GetComponent<CashPool>();
+        twoHandSwordPool = transform.GetChild(3).GetComponent<TwoHandSwordPool>();
+        bowPool = transform.GetChild(4).GetComponent<BowPool>();
+        wandPool = transform.GetChild(5).GetComponent<WandPool>();
+        daggerPool = transform.GetChild(6).GetComponent<DaggerPool>();
+        twoHandAxePool = transform.GetChild(7).GetComponent<TwoHandAxePool>();
+        batPool = transform.GetChild(8).GetComponent<BatPool>();
+
+
 
         hpPotionpool.Initialize();
+        mpPotionPool.Initialize();
+        cashPool.Initialize();
+        twoHandSwordPool.Initialize();
+        bowPool.Initialize();
+        wandPool.Initialize();
+        daggerPool.Initialize();
+        twoHandSwordPool.Initialize();
+        twoHandAxePool.Initialize();
+        batPool.Initialize();
     }
     public GameObject GetObject(Pool_Object_Type type)
     {
@@ -48,31 +74,31 @@ public class Factory : MonoBehaviour
         switch (type)
         {
             case Pool_Object_Type.Cash:
-                result = hpPotionpool?.GetObject()?.gameObject;
+                result = cashPool?.GetObject()?.gameObject;
                 break;
             case Pool_Object_Type.HpPotion:
                 result = hpPotionpool?.GetObject()?.gameObject;
                 break;
             case Pool_Object_Type.MpPotion:
-                result = hpPotionpool?.GetObject()?.gameObject;
+                result = mpPotionPool?.GetObject()?.gameObject;
                 break;
             case Pool_Object_Type.Sword:
-                result = hpPotionpool?.GetObject()?.gameObject;
+                result = twoHandSwordPool?.GetObject()?.gameObject;
                 break;
             case Pool_Object_Type.Wand:
-                result = hpPotionpool?.GetObject()?.gameObject;
+                result = wandPool?.GetObject()?.gameObject;
                 break;
             case Pool_Object_Type.Bow:
-                result = hpPotionpool?.GetObject()?.gameObject;
+                result = bowPool?.GetObject()?.gameObject;
                 break;
             case Pool_Object_Type.Axe:
-                result = hpPotionpool?.GetObject()?.gameObject;
+                result = twoHandAxePool?.GetObject()?.gameObject;
                 break;
             case Pool_Object_Type.Bat:
-                result = hpPotionpool?.GetObject()?.gameObject;
+                result = batPool?.GetObject()?.gameObject;
                 break;
             case Pool_Object_Type.Dagger:
-                result = hpPotionpool?.GetObject()?.gameObject;
+                result = daggerPool?.GetObject()?.gameObject;
                 break;
 
             default:
