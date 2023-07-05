449d487cfa21273dec6612ae60afb74328712c6b
PieroKim13 <113963977+PieroKim13@users.noreply.github.com>
2023-07-05 17:21:58 +0900

    Test
    Test

---
+++ Assets/Scenes/Kim/Script/EnemyBase.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    public float hp;    // Ã¼·Â
+    public float str;   // Èû
+    public float dex;   // ¹ÎÃ¸
+    public float lux;   // ¿î
+
+    
+}
