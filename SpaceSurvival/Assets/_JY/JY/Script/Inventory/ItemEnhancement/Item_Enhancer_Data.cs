76f06223efe1b6b86927ca129585f49f047d75fd
steelfs <steelfs7@gmail.com>
2023-07-30 20:55:51 +0900

    ItemEnhancer 제작중

---
+++ SpaceSurvival/Assets/_JY/JY/Script/Inventory/ItemEnhancement/Item_Enhancer_Data.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Enhancer_Data : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ SpaceSurvival/Assets/_JY/JY/Script/Inventory/ItemEnhancement/Item_Enhancer_UI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_Enhancer_UI : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
