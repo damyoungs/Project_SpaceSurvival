fdbf5c7a1d01f254f0ca36f57a1fb012aa7398d0
SengbumKim <133838075+SengbumKim@users.noreply.github.com>
2023-08-01 15:12:17 +0900

    beforeSlot에 드롭시 장비아이템이 아니면 return처리
    Ray를 쏴서 비교하지 않고 위치로 비교해서 영역안에 있는지 판단

--- SpaceSurvival/Assets/_JY/JY/Script/Inventory/ItemEnhancement/Enhancer_Slot_After.cs
+++ SpaceSurvival/Assets/_JY/JY/Script/Inventory/ItemEnhancement/Enhancer_Slot_After.cs
@@ -11,4 +11,10 @@ public class Enhancer_Slot_After : Enhancer_Slot_Base
     {
         GameManager.SlotManager.setEnhanceItem += (itemData) => ItemData = itemData;
     }
+    protected override void Refresh()
+    {
+        //UI에 보여지는 부분 업데이트
+        //EnhancerDetail 업데이트
+        base.Refresh();
+    }
 }
