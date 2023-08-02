d8f432db7e8579c545abbeea50fac64c678c8b4c
inchimyeong <125725323+steelfs@users.noreply.github.com>
2023-08-02 11:28:35 +0900

    Update Item_Enhancer.cs

--- SpaceSurvival/Assets/_JY/JY/Script/Inventory/ItemEnhancement/Item_Enhancer.cs
+++ SpaceSurvival/Assets/_JY/JY/Script/Inventory/ItemEnhancement/Item_Enhancer.cs
@@ -2,6 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EnhancerState
+{
+    Open,
+    SetItem,
+    WaitforResult,
+    Success,
+    Fail,
+    ClearItem,
+    Close
+}
 public class Item_Enhancer : MonoBehaviour
 {
     Item_Enhancer_UI item_Enhancer_UI;
@@ -10,6 +20,37 @@ public class Item_Enhancer : MonoBehaviour
     public Item_Enhancer_UI EnhancerUI => item_Enhancer_UI;
     public Item_Enhancer_Data EnhancerData => item_Enhancer_Data;
 
+    EnhancerState enhancerState;
+    public EnhancerState EnhancerState
+    {
+        get => enhancerState;
+        set
+        {
+            if (enhancerState != value)
+            {
+                enhancerState = value;
+            }
+            switch (enhancerState)
+            {
+                case EnhancerState.Open:
+                    break;
+                case EnhancerState.SetItem:
+                    break;
+                case EnhancerState.WaitforResult:
+                    break;
+                case EnhancerState.Success:
+                    break;
+                case EnhancerState.Fail:
+                    break;
+                case EnhancerState.ClearItem:
+                    break;
+                case EnhancerState.Close:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
     private void Awake()
     {
         item_Enhancer_UI = GetComponent<Item_Enhancer_UI>();
