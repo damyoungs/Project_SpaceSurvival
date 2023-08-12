57de04a1791ca6f147e5a6035068ed402d089723
steelfs <steelfs7@gmail.com>
2023-08-12 22:32:37 +0900

    기존 Bullet 삭제전 백업

--- SpaceSurvival/Assets/_JY/JY/Script/UI/ItemDescription.cs
+++ SpaceSurvival/Assets/_JY/JY/Script/UI/ItemDescription.cs
@@ -41,6 +41,7 @@ public class ItemDescription : MonoBehaviour
         {
             ItemData_Equip equipItem = data as ItemData_Equip;
             ItemData_Enhancable enhancableItem = data as ItemData_Enhancable;
+            ItemData_Bullet bullet = data as ItemData_Bullet;
             if (enhancableItem != null)
             {
                 itemDetail.text = $"공격력 : {equipItem.attackPoint} \n방어력 : {equipItem.defencePoint}\n아이템 레벨 : {enhancableItem.itemLevel}";
@@ -48,6 +49,10 @@ public class ItemDescription : MonoBehaviour
             else if (equipItem != null)
             {
                 itemDetail.text = $"공격력 : {equipItem.attackPoint} \n방어력 : {equipItem.defencePoint}";
+            }
+            else if (bullet != null)
+            {
+                itemDetail.text = $"공격력 : {bullet.attckPoint}\n{bullet.itemDescription}";
             }
             else
             {
