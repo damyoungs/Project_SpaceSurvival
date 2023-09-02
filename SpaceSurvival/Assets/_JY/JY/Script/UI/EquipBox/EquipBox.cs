8a9d5e6af75cc40729d3af05f99c011d741bc96c
steelfs <steelfs7@gmail.com>
2023-09-02 21:36:11 +0900

    아이템 장착시 프리팹 붙히는 중

--- SpaceSurvival/Assets/_JY/JY/Script/UI/EquipBox/EquipBox.cs
+++ SpaceSurvival/Assets/_JY/JY/Script/UI/EquipBox/EquipBox.cs
@@ -100,6 +100,7 @@ public class EquipBox : MonoBehaviour
     void UnEquip_Item(ItemData itemData)
     {
         on_Update_Status_For_UnEquip?.Invoke(itemData);
+        Remove_Prefab(itemData);
         EquipBox_Slot slot = Find_Slot_By_Type(itemData);
         slot.ItemData = null;
     }
@@ -109,11 +110,35 @@ public class EquipBox : MonoBehaviour
         if (slot != null)
         {
             GameManager.SlotManager.Just_ChangeSlot.ItemData = null;
-            on_Update_Status_For_EquipOrSwap?.Invoke(slot.ItemData, itemData);//장비중이 아닐 때는 첫번째 파라미터가 null 이 전달 된다.
-            //ItemData 두개를 파라미터로 델리게이트 신호 보내고 플레이어에서 before, after 받아서 공격력, 방어력 반영
-            slot.SetItemData(itemData);
+            on_Update_Status_For_EquipOrSwap?.Invoke(slot.ItemData, itemData);//장비중이 아닐 때는 첫번째 파라미터가 null 이 전달 된다. // 플레이어 공격력, 방어력 셋팅
+            Attach_Prefab(itemData);//프리팹 부착
+            slot.SetItemData(itemData);//장비슬롯 UI업데이트
         }
 
+    }
+    void Remove_Prefab(ItemData data)
+    {
+        Transform parentTransform = GetParentTransform(data);
+        GameObject itemPrefab = parentTransform.GetChild(0).gameObject;
+        Destroy(itemPrefab);
+    }
+    void Attach_Prefab(ItemData data)
+    {
+        Transform parentTransform = GetParentTransform(data);
+        if (parentTransform.transform.childCount > 0)// 이미 부착되어있는 아이템이 있으면 제거 후 장착
+        {
+            GameObject itemPrefab = parentTransform.GetChild(0).gameObject;
+            Destroy(itemPrefab);
+            GameObject newItemPrefab = Instantiate(data.modelPrefab, parentTransform);
+            newItemPrefab.transform.localPosition = Vector3.zero;
+            newItemPrefab.transform.localRotation = Quaternion.identity;
+        }
+        else
+        {
+            GameObject itemPrefab = Instantiate(data.modelPrefab, parentTransform);
+            itemPrefab.transform.localPosition = Vector3.zero;
+            itemPrefab.transform.localRotation = Quaternion.identity;
+        }
     }
     EquipBox_Slot Find_Slot_By_Type(ItemData itemData)
     {
@@ -159,6 +184,31 @@ public class EquipBox : MonoBehaviour
         }
         Debug.Log(equipSlot);
         return equipSlot;
+    }
+    Transform GetParentTransform(ItemData data)
+    {
+        Transform result = null;
+        ItemData_Armor armor = data as ItemData_Armor;
+        ItemData_Hat hat = data as ItemData_Hat;
+        ItemData_Craft jewel = data as ItemData_Craft;
+        ItemData_Enhancable weapon = data as ItemData_Enhancable;
+        if (hat != null)
+        {
+            result = equip_Parent_Transform[0];
+        }
+        else if (armor != null)
+        {
+            result = equip_Parent_Transform[2];
+        }
+        else if (weapon != null)
+        {
+            result = equip_Parent_Transform[1];
+        }
+        else if (jewel != null)
+        {
+            result = equip_Parent_Transform[3];
+        }
+        return result;
     }
     public void Open()
     {
