dc3ffa54098f14ed5db74bde4fc38584114a4e0c
damyoungs <133838199+damyoungs@users.noreply.github.com>
2023-09-04 14:43:22 +0900

    일단초기화

--- SpaceSurvival/Assets/_JY/JY/Script/Inventory/ItemData(ScriptableObject)/Interface/IEnhancable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IEnhancable 
-{
-    bool LevelUp(uint darkForceCount);
-    float CalculateSuccessRate(uint darkForceCount);
-}
--- SpaceSurvival/Assets/_JY/JY/Script/UI/QuickSlot/TempSlot_For_QuickSlot.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TempSlot_For_QuickSlot : TempSlot_For_QuickSlot_Base
-{
-
-}
