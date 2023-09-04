be4271926a8cb088373959edf6207f3ff278de1d
SengbumKim <133838075+SengbumKim@users.noreply.github.com>
2023-09-04 15:20:10 +0900

    전투중 키 인풋 막기

--- SpaceSurvival/Assets/_Yoon_Dae_Myeong/_Scripts/_Unit/ShootAction.cs
+++ SpaceSurvival/Assets/_Yoon_Dae_Myeong/_Scripts/_Unit/ShootAction.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,15 @@ public class ShootAction : PlayerMove
 	[SerializeField]
 	private Transform shootPointTransform;
 
+	public Action onAttack;
     //무기 바뀔 때마다 shootPointTransform 수정
 	//총알 생성을 여기서 하는게 맞는건가?
 	//장착까지만
     protected override void onClickRight(InputAction.CallbackContext context)
 	{
-		unitAnimator.SetTrigger("IsFiring");
+		onAttack?.Invoke();
+
+        unitAnimator.SetTrigger("IsFiring");
 		Instantiate(bulletProjectilePrefab, shootPointTransform.position, shootPointTransform.rotation);
 		audioSource.Play();
 	}
