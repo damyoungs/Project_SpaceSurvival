f42a914f63cf795e95a9523db6aacb1ad93e0bc7
bgsg1307 <bg1307@naver.com>
2023-09-07 01:19:58 +0900

    플레이어 움직이기

--- SpaceSurvival/Assets/_Cho_Seung_Geun/Scripts/Cho_PlayerMove.cs
+++ SpaceSurvival/Assets/_Cho_Seung_Geun/Scripts/Cho_PlayerMove.cs
@@ -1,18 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Cho_PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float walkSpeed = 3.5f;
+    public float runSpeed = 5.0f;
+    public float jumpHeight = 5.0f;
+
+
+    float speed = 0.0f;
+    Vector3 moveDir = Vector3.zero;
+
+    InputKeyMouse inputActions;
+    Rigidbody rigid;
+
+    private void Awake()
     {
-        
+        inputActions = new InputKeyMouse();
+        rigid = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        speed = walkSpeed;
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+        inputActions.Player.Jump.performed += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Jump.performed -= OnJump;
+        inputActions.Player.Move.canceled -= OnMove;
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Disable();
+    }
+
+
+    private void Update()
+    {
+        rigid.MovePosition(rigid.transform.position + Time.fixedDeltaTime * speed * moveDir);
+    }
+
+    private void OnMove(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        Vector2 dir = context.ReadValue<Vector2>();
+
+        moveDir.x = dir.x;
+        moveDir.z = dir.y;
+    }
+
+    private void OnJump(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        rigid.AddForce(jumpHeight * Vector3.up, ForceMode.Impulse);
     }
 }
