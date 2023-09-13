f5dd188e2d299d17ea5a1d721a0586c3f2a42e6c
SengbumKim <133838075+SengbumKim@users.noreply.github.com>
2023-09-13 15:21:32 +0900

    skillData작성중

--- SpaceSurvival/Assets/_JY/JY/Script/SkillBox/SkillData.cs
+++ SpaceSurvival/Assets/_JY/JY/Script/SkillBox/SkillData.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillData : MonoBehaviour
 {
-    string skillName;   public string SkillName { get => skillName; protected set { skillName = value; } }
-    byte skillLevel;    public byte SkillLevel { get => skillLevel; protected set { skillLevel = value; } }
-    AnimationClip animClip;  public AnimationClip AnimClip { get => animClip; protected set {  animClip = value; } }
-    AudioClip audioClip;    public AudioClip AudioClip { get => audioClip; protected set { audioClip = value; } }
-    byte attackRange;
-    short require_Force_For_skillLevelUp;
-    byte require_Stamina_For_UsingSkill;
+    string skillName;                        public string SkillName { get => skillName; protected set { skillName = value; } }
+    int skillLevel;                          public int SkillLevel { get => skillLevel; protected set { skillLevel = value; } }
+    AnimationClip animClip;                  public AnimationClip AnimClip { get => animClip; protected set {  animClip = value; } }
+    AudioClip audioClip;                     public AudioClip AudioClip { get => audioClip; protected set { audioClip = value; } }
+    int attackRange;                         public int AttackRange { get => attackRange; protected set { attackRange = value; } }
+    int require_Force_For_skillLevelUp;      public int Require_Force_For_skillLevelUp { get => require_Force_For_skillLevelUp; protected set { require_Force_For_skillLevelUp = value; } }
+    int require_Stamina_For_UsingSkill;      public int Require_Stamina_For_UsingSkill { get => require_Stamina_For_UsingSkill; protected set { require_Stamina_For_UsingSkill = value; } }
 
+
+    //UI
+    protected Image itemicon;
+    protected Button button;
+    protected TextMeshProUGUI skillLevel_Text;
+    //#EndUI
     protected virtual void Init()
     {
-
+        itemicon = transform.GetChild(1).GetChild(0).GetComponent<Image>();
+        button = transform.GetChild(2).GetComponent<Button>();
+        skillLevel_Text = transform.GetChild(3).GetComponent<TextMeshProUGUI>();
     }
 }
--- SpaceSurvival/Assets/_JY/JY/Script/SkillBox/Skill_05.cs
+++ SpaceSurvival/Assets/_JY/JY/Script/SkillBox/Skill_05.cs
@@ -6,6 +6,13 @@ public class Skill_05 : SkillData
 {
     protected override void Init()
     {
-        
+        base.Init();
+        SkillName = "일반 공격";
+        SkillLevel = 0;
+        //AnimClip
+        //audioClip
+        AttackRange = 1;
+        Require_Force_For_skillLevelUp = 2;
+        Require_Stamina_For_UsingSkill = 2;
     }
 }
