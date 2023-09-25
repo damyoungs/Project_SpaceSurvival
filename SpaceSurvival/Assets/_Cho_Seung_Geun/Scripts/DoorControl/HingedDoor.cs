7aeb6e2dacfaa2069e5fc57fe531dfbdeb3859e5
bgsg1307 <bg1307@naver.com>
2023-09-26 01:24:23 +0900

    콜라이더 수정(5)

--- SpaceSurvival/Assets/_Cho_Seung_Geun/Scripts/DoorControl/HingedDoor.cs
+++ SpaceSurvival/Assets/_Cho_Seung_Geun/Scripts/DoorControl/HingedDoor.cs
@@ -19,18 +19,42 @@ public class HingedDoor : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            StopAllCoroutines();
             StartCoroutine(Open());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StopAllCoroutines();
+            StartCoroutine(Close());
         }
     }
 
     IEnumerator Open()
     {
         doorCollider.enabled = false;
-        while (door.rotation.eulerAngles.y < 120.0f)
+        while (door.localRotation.eulerAngles.y < 120.0f)
         {
             door.rotation *= Quaternion.Euler(0.0f, Time.deltaTime * speed, 0.0f);
             yield return null;
         }
+        doorCollider.enabled = true;
+    }
+
+    IEnumerator Close()
+    {
+        doorCollider.enabled = false;
+        float rotate = Time.deltaTime * speed;
+
+        while (door.localRotation.eulerAngles.y > rotate)
+        {
+            door.rotation *= Quaternion.Euler(0.0f, -rotate, 0.0f);
+            yield return null;
+        }
+        door.localRotation = Quaternion.Euler(door.localRotation.x, 0.0f, door.localRotation.z);
         doorCollider.enabled = true;
     }
 }
