17900e7e64831a340b6fb450d407f24000d364e8
kaijok87 <Kaijok@naver.com>
2023-10-04 15:10:17 +0900

    머지

--- SpaceSurvival/Assets/_Noh_Hyun_Oh/Scripts/BattleShip/BattleShipInitData.cs
+++ SpaceSurvival/Assets/_Noh_Hyun_Oh/Scripts/BattleShip/BattleShipInitData.cs
@@ -14,25 +14,25 @@ public class BattleShipInitData : MonoBehaviour
         questManager.InitDataSetting();
         merchantManager.InitDataSetting();
     }
-    private void OnEnable()
-    {
-        InputSystemController.InputSystem.Common.Esc.performed += (_)=> EscOnAction();
-        InputSystemController.InputSystem.Mouse.MouseClick.performed += (_)=> EscOffAction();
-    }
+    //private void OnEnable()
+    //{
+    //    InputSystemController.InputSystem.Common.Esc.performed += (_)=> EscOnAction();
+    //    InputSystemController.InputSystem.Mouse.MouseClick.performed += (_)=> EscOffAction();
+    //}
 
 
-    private void OnDestroy()
-    {
-        InputSystemController.InputSystem.Mouse.MouseClick.performed -= (_)=> EscOffAction();
-        InputSystemController.InputSystem.Common.Esc.performed -= (_)=> EscOnAction();
-    }
-    private void EscOnAction()
-    {
-        Cursor.lockState = CursorLockMode.None;
-    }
-    private void EscOffAction()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+    //private void OnDestroy()
+    //{
+    //    InputSystemController.InputSystem.Mouse.MouseClick.performed -= (_)=> EscOffAction();
+    //    InputSystemController.InputSystem.Common.Esc.performed -= (_)=> EscOnAction();
+    //}
+    //private void EscOnAction()
+    //{
+    //    Cursor.lockState = CursorLockMode.None;
+    //}
+    //private void EscOffAction()
+    //{
+    //    Cursor.lockState = CursorLockMode.Locked;
+    //}
 
 }
