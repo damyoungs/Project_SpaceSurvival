bf191f50ca0bf88a15d717c9eb459fd28e0a33c0
gmldyddl123 <119833234+gmldyddl123@users.noreply.github.com>
2023-10-05 15:21:14 +0900

    포탈 이동 구현

--- SpaceSurvival/Assets/_Cho_Seung_Geun/Scripts/Cho_SceneLoading.cs
+++ SpaceSurvival/Assets/_Cho_Seung_Geun/Scripts/Cho_SceneLoading.cs
@@ -8,10 +8,17 @@ public class Cho_SceneLoading : MonoBehaviour
     InteractionUI interaction;
     Cho_PlayerMove player;
 
+    ParticleSystem shortRay;
+    ParticleSystem longRay;
+
     private void Awake()
     {
         interaction = FindObjectOfType<InteractionUI>();
         player = FindObjectOfType<Cho_PlayerMove>();
+        Transform parent = transform.parent;
+        shortRay = parent.GetChild(0).GetComponent<ParticleSystem>();
+        longRay = parent.GetChild(5).GetComponent<ParticleSystem>();
+        longRay.Stop();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -34,6 +41,20 @@ public class Cho_SceneLoading : MonoBehaviour
 
     private void Warp()
     {
+        //player.transform.position = shortRay.transform.position;
+        StartCoroutine(WarpCoroutine());
+    }
+
+    // ¼öÁ¤ ÇØ¾ßµÊ
+    IEnumerator WarpCoroutine()
+    {
+        player.Cinemachine.Priority = 20;
+        shortRay.Stop();
+        longRay.Play();
+        yield return new WaitForSeconds(3.0f);
+        player.Cinemachine.Priority = -10;
+        shortRay.Play();
+        longRay.Stop();
         LoadingScene.SceneLoading(EnumList.SceneName.TestBattleMap);
     }
 }
--- SpaceSurvival/Assets/_Cho_Seung_Geun/Scripts/UI/InteractionUI.cs
+++ SpaceSurvival/Assets/_Cho_Seung_Geun/Scripts/UI/InteractionUI.cs
@@ -8,16 +8,12 @@ public class InteractionUI : MonoBehaviour
     public Action visibleUI;
     public Action invisibleUI;
 
-    float speed = 20.0f;
-
-    CanvasGroup canvasGroup;
     Animator animator;
 
     readonly int Hash_Visible = Animator.StringToHash("IsVisible");
 
     private void Awake()
     {
-        canvasGroup = GetComponent<CanvasGroup>();
         animator = GetComponent<Animator>();
         visibleUI = OnVisible;
         invisibleUI = OnInvisible;
@@ -25,34 +21,11 @@ public class InteractionUI : MonoBehaviour
 
     void OnVisible()
     {
-        //StartCoroutine(VisibleUI());
         animator.SetBool(Hash_Visible, true);
     }
 
     void OnInvisible()
     {
-        //StartCoroutine(InvisibleUI());
         animator.SetBool(Hash_Visible, false);
     }
-
-    IEnumerator VisibleUI()
-    {
-        while (canvasGroup.alpha < 1)
-        {
-            canvasGroup.alpha += Time.deltaTime * speed;
-            yield return null;
-        }
-        Debug.Log("ui 보임");
-    }
-
-    IEnumerator InvisibleUI()
-    {
-        while (canvasGroup.alpha > 0)
-        {
-            canvasGroup.alpha -= Time.deltaTime * speed;
-            yield return null;
-        }
-        Debug.Log("ui 안 보임");
-    }
-
 }
