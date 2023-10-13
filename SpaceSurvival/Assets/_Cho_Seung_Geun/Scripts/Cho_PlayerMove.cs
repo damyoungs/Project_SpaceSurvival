f53445c404eb56687c72f7ca7b38e59eb895bb72
gmldyddl123 <119833234+gmldyddl123@users.noreply.github.com>
2023-10-13 13:22:51 +0900

    이동 수정

--- SpaceSurvival/Assets/_Cho_Seung_Geun/Scripts/Cho_PlayerMove.cs
+++ SpaceSurvival/Assets/_Cho_Seung_Geun/Scripts/Cho_PlayerMove.cs
@@ -38,7 +38,8 @@ public class Cho_PlayerMove : MonoBehaviour
 
     Vector3 moveDir = Vector3.zero;
     float curRotateY = 0.0f;
-    bool isJumping = false;
+    float jumpCheckHeight = 0.0f;
+    bool jumpChecking = false;
     int jumpCount = 0;
 
     PlayerState state = PlayerState.Idle;
@@ -50,6 +51,14 @@ public class Cho_PlayerMove : MonoBehaviour
             if (state != value)
             {
                 state = value;
+                if (state == PlayerState.Walk || state == PlayerState.Run)
+                {
+                    audios[(int)AudioIndex.Walk].Play();
+                }
+                else
+                {
+                    audios[(int)AudioIndex.Walk].Stop();
+                }
             }
         }
     }
@@ -152,7 +161,6 @@ public class Cho_PlayerMove : MonoBehaviour
         {
             animator.SetFloat(Hash_InputX, dir.x);
             animator.SetFloat(Hash_InputY, dir.y);
-        }
 
         if (context.performed)
         {
@@ -160,21 +168,53 @@ public class Cho_PlayerMove : MonoBehaviour
             {
                 State = PlayerState.Run;
                 animator.SetFloat(Hash_Speed, animatorRunSpeed);
+                animator.SetBool(Hash_IsRun, true);
             }
             else if (speed > walkSpeed - 0.01f)
             {
                 State = PlayerState.Walk;
                 animator.SetFloat(Hash_Speed, animatorWalkSpeed);
+                animator.SetBool(Hash_IsRun, false);
             }
-            audios[(int)AudioIndex.Walk].Play();
+            //audios[(int)AudioIndex.Walk].Play();
         }
         else
         {
             State = PlayerState.Idle;
             animator.SetFloat(Hash_Speed, 0);
-            audios[(int)AudioIndex.Walk].Stop();
+            //audios[(int)AudioIndex.Walk].Stop();
+        }
         }
 
+    }
+
+    private void OnDash(InputAction.CallbackContext context)
+    {
+        if (jumpCount == 0)
+        {
+            if (context.canceled)
+            {
+                speed = walkSpeed;
+                if (State == PlayerState.Run)
+                {
+                    audios[(int)AudioIndex.Walk].pitch = 1.26f;
+                    State = PlayerState.Walk;
+                    animator.SetFloat(Hash_Speed, animatorWalkSpeed);
+                    animator.SetBool(Hash_IsRun, false);
+                }
+            }
+            else
+            {
+                speed = runSpeed;
+                if (State == PlayerState.Walk)
+                {
+                    audios[(int)AudioIndex.Walk].pitch = 2.0f;
+                    State = PlayerState.Run;
+                    animator.SetFloat(Hash_Speed, animatorRunSpeed);
+                    animator.SetBool(Hash_IsRun, true);
+                }
+            }
+        }
     }
 
     private void OnJump(UnityEngine.InputSystem.InputAction.CallbackContext context)
@@ -183,40 +223,15 @@ public class Cho_PlayerMove : MonoBehaviour
         {
             //audios[(int)AudioIndex.Jump].Play();
             moveDir.y = jumpHeight;
-            //isJumping = true;
+            State = PlayerState.Jump;
+            jumpCheckHeight = transform.position.y + controller.radius * 2;
+            jumpChecking = true;
             animator.SetTrigger(Hash_IsJump);
             animator.SetBool(Hash_IsJumping, true);
             jumpCount++;
         }
     }
-
-    private void OnDash(InputAction.CallbackContext context)
-    {
-
-        if (context.canceled)
-        {
-            speed = walkSpeed;
-            if (State == PlayerState.Run)
-            {
-                State = PlayerState.Walk;
-                audios[(int)AudioIndex.Walk].pitch = 1.26f;
-                animator.SetFloat(Hash_Speed, animatorWalkSpeed);
-                animator.SetBool(Hash_IsRun, false);
-            }
-        }
-        else
-        {
-            speed = runSpeed;
-            if (State == PlayerState.Walk)
-            {
-                State = PlayerState.Run;
-                audios[(int)AudioIndex.Walk].pitch = 2.0f;
-                animator.SetFloat(Hash_Speed, animatorRunSpeed);
-                animator.SetBool(Hash_IsRun, true);
-            }
-        }
-    }
-
+    
     private void OnInteract(InputAction.CallbackContext context)
     {
         interaction?.Invoke();
@@ -236,21 +251,28 @@ public class Cho_PlayerMove : MonoBehaviour
 
     private bool IsGrounded()
     {
+        if (jumpChecking && transform.position.y > jumpCheckHeight)
+        {
+            jumpChecking = false;
+        }
         Vector3 groundCheckPosition = new Vector3(transform.position.x, transform.position.y + controller.radius * 0.5f, transform.position.z);
         if (Physics.CheckSphere(groundCheckPosition, controller.radius, LayerMask.GetMask("Ground")))
         {
-            if (moveDir.y < jumpHeight)
+            if (!jumpChecking)
             {
-                moveDir.y = -0.01f;
+                if (moveDir.y < jumpHeight)
+                {
+                    moveDir.y = -0.01f;
+                }
+                if (jumpCount > 0)
+                {
+                    animator.SetBool(Hash_IsJumping, false);
+                    audios[(int)AudioIndex.JumpLanding].Play();
+                }
+                jumpChecking = false;
+                jumpCount = 0;
+                return true;
             }
-            if (jumpCount > 0)
-            {
-                animator.SetBool(Hash_IsJumping, false);
-                audios[(int)AudioIndex.JumpLanding].Play();
-            }
-            //isJumping = false;
-            jumpCount = 0;
-            return true;
         }
 
         return false;
@@ -260,7 +282,7 @@ public class Cho_PlayerMove : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y + 0.25f, transform.position.z), 0.25f);
+        Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y + 0.125f, transform.position.z), 0.25f);
     }
 
 #endif
